0cbecc0b57f2006c23649676247a0bc40566fbec
max <max@SYDCO-LMBA-2.local>
2021-04-30 22:44:32 +1000

    heroku only

--- build_boost.sh
+++ build_boost.sh
@@ -1,6 +1,11 @@
-cd /tmp
-wget https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.bz2
-tar --bzip2 -xf boost_1_61_0.tar.bz2
-cd boost_1_61_0/
-./bootstrap.sh
-./b2 cxxflags=-fPIC cflags=-fPIC -a link=static
+# only on heroku for now
+if [[ -z "${HEROKU_ENV}" ]]; then
+
+    cd /tmp
+    wget https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.bz2
+    tar --bzip2 -xf boost_1_61_0.tar.bz2
+    cd boost_1_61_0/
+    ./bootstrap.sh
+    ./b2 cxxflags=-fPIC cflags=-fPIC -a link=static
+
+fi
--- build_python.sh
+++ build_python.sh
@@ -1,24 +1,29 @@
-# build python
+# only on heroku for now
+if [[ -z "${HEROKU_ENV}" ]]; then
 
-git clone --depth 1 --branch v3.8.5 https://github.com/python/cpython
+    # build python
 
-echo "*static*
-math mathmodule.c _math.c" > Modules/Setup.local
-# check Modules/Setup if anything missing
+    git clone --depth 1 --branch v3.8.5 https://github.com/python/cpython
 
-# on MAC
-if [ "$(uname)" == "Darwin" ]; then
-    sudo xcode-select --install
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    brew install git openssl readline
-    export PKG_CONFIG_PATH="$(brew --prefix openssl)/lib/pkgconfig"
-    export CPPFLAGS="-I$(brew --prefix readline)/include"
-    export LDFLAGS="-L$(brew --prefix readline)/lib" 
+    echo "*static*
+    math mathmodule.c _math.c" > Modules/Setup.local
+    # check Modules/Setup if anything missing
+
+    # on MAC
+    if [ "$(uname)" == "Darwin" ]; then
+        sudo xcode-select --install
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        brew install git openssl readline
+        export PKG_CONFIG_PATH="$(brew --prefix openssl)/lib/pkgconfig"
+        export CPPFLAGS="-I$(brew --prefix readline)/include"
+        export LDFLAGS="-L$(brew --prefix readline)/lib" 
+    fi
+
+    # all
+    ./configure --disable-shared
+
+    make # add -j if not heroku
+
+    # ./libpython3.8.a is ready
+
 fi
-
-# all
-./configure --disable-shared
-
-make # add -j if not heroku
-
-# ./libpython3.8.a is ready

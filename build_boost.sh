85a0c2e1cda02a377d3ff3d233000049615299a9
max <max@SYDCO-LMBA-2.local>
2021-04-30 16:07:58 +1000

    minor

--- build_boost.sh
+++ build_boost.sh
@@ -1,3 +1,4 @@
+cd /tmp
 wget https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.bz2
 tar --bzip2 -xf boost_1_61_0.tar.bz2
 cd boost_1_61_0/
--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-const nativeAddonPath = require.resolve(`./prebuilds/linux-x64/node.napi.node`);
+const nativeAddonPath = require.resolve(
+  `./prebuilds/${process.platform}-x64/node.napi.node`
+);
 const nativeAddon = require(nativeAddonPath);
 const text = process.argv[2] || "Woohooo 😍 ✌️";
 console.log(`nativeAddon( ${text} ) = ${nativeAddon(text)}`);

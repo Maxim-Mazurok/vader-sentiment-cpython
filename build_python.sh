f74ef870fd8560a29fe82b135537301640380f43
max <max@SYDCO-LMBA-2.local>
2021-04-30 22:23:22 +1000

    build

--- build_python.sh
+++ build_python.sh
@@ -6,6 +6,17 @@ echo "*static*
 math mathmodule.c _math.c" > Modules/Setup.local
 # check Modules/Setup if anything missing
 
+# on MAC
+if [ "$(uname)" == "Darwin" ]; then
+    sudo xcode-select --install
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install git openssl readline
+    export PKG_CONFIG_PATH="$(brew --prefix openssl)/lib/pkgconfig"
+    export CPPFLAGS="-I$(brew --prefix readline)/include"
+    export LDFLAGS="-L$(brew --prefix readline)/lib" 
+fi
+
+# all
 ./configure --disable-shared
 
 make -j

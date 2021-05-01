96ceb18a70e13a25146032baa11192f329fa91d5
Maxim Mazurok <maxim@mazurok.com>
2021-05-02 01:29:42 +1000

    test

--- node-gyp-build.sh
+++ node-gyp-build.sh
@@ -4,13 +4,10 @@ cd ${SCRIPT_DIR}
 
 if npm run node-gyp-build-test; then
     echo "using prebuild"
-elif [ "$(python3 -V)" == "Python 3.8.5" ]; then
+else
     # only works for Python 3.8.5 for now
     echo "no prebuild, rebuilding..."
     bash ./build_boost.sh && bash ./build_python.sh
-else
-    echo "no prebuild, no Python 3.8.5, can't rebuild... make sure that `python3 -V` returns `Python 3.8.5`"
-    exit 1
 fi
 
 npm run node-gyp-build

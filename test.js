63ea9369404637dba18d601bab6a12a0b7738741
Maxim Mazurok <maxim@mazurok.com>
2021-05-01 14:32:55 +1000

    test index

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
-const nativeAddonPath = require.resolve(
-  `./prebuilds/${process.platform}-x64/node.napi.node`
-);
-const nativeAddon = require(nativeAddonPath);
+const nativeAddon = require("./index");
 const text = process.argv[2] || "Woohooo 😍 ✌️";
 console.log(`nativeAddon( ${text} ) = ${nativeAddon(text)}`);

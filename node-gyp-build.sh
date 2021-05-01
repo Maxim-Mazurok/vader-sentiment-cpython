9de5e1c802d7e905466b14c7e36db19787a6148d
max <max@SYDCO-LMBA-2.local>
2021-05-01 22:31:18 +1000

    node-gyp-build

--- node-gyp-build.sh
+++ node-gyp-build.sh
@@ -1,6 +1,16 @@
 # only on heroku for now
 if [[ "${HEROKU_ENV}" ]]; then
 
-    ./node_modules/.bin/node-gyp-build
+    ls -la
+
+    pwd
+
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+    echo ${SCRIPT_DIR}
+
+    cd ${SCRIPT_DIR}
+
+    npm run node-gyp-build
 
 fi
